1542f16ab3d55cd4fe6932947f3124098b8e1f02
SennovalabCaldas <yanethmejia@ariadnacg.com>
2024-04-03 18:06:38 -0500

    Initial structure

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('categories', CategoryController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
